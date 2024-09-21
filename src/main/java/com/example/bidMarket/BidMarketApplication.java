76988cb13f9b37b31e7c77da55681c4fdf83be9c
Quân Nguyễn <quanlh3052004@gmail.com>
2024-09-21 02:59:51 +0100

    Start project

---
+++ src/main/java/com/example/bidMarket/BidMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.bidMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidMarketApplication.class, args);
+	}
+
+}
