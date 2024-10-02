329566ba1263f247603f0c0927c5fcdbb1425a56
Quân Nguyễn <quanlh3052004@gmail.com>
2024-10-02 12:33:43 +0100

    Ticket-11 (Vuong) : Add advance exception class and refactor code

--- src/main/java/com/example/bidMarket/KafkaService/BidConsumer.java
+++ src/main/java/com/example/bidMarket/KafkaService/BidConsumer.java
@@ -1,20 +1,15 @@
 package com.example.bidMarket.KafkaService;
 
 import com.example.bidMarket.dto.Request.BidCreateRequest;
-import com.example.bidMarket.model.Auction;
-import com.example.bidMarket.repository.BidRepository;
-import com.example.bidMarket.service.AuctionService;
 import com.example.bidMarket.service.BidService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 @Slf4j
-public class KafkaConsumer {
+public class BidConsumer {
 
     @Autowired
     private BidService bidService;
